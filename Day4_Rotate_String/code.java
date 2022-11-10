95da8360a5f13314f36d7a5c8b943c9496c3708c
Sourav Kumar <31382363+studentdevelops@users.noreply.github.com>
2022-11-10 23:26:31 +0530

    Create code.java

---
+++ Day4_Rotate_String/code.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length())
+        return false;
+
+        if( (s+s).indexOf(goal) >= 0 ){
+            return true;
+        } 
+        return false;
+    }
+}
