2a19daafb0c7d41b5dab6fc02f7a8e44c6af6513
Sourav Kumar <31382363+studentdevelops@users.noreply.github.com>
2022-11-11 23:52:57 +0530

    Update alternativeCode.java

--- Day5_Valid_Palindrome/alternativeCode.java
+++ Day5_Valid_Palindrome/alternativeCode.java
@@ -1,13 +1,13 @@
+// Solution too slow to be accepted due to the time taken by substring()
+
 class Solution {
 public static boolean isPalindrome(String s) {
         String filteredString = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         if (filteredString.length() == 1 || filteredString.length() == 0) {
             return true;
         }
-        if (filteredString.charAt(0) == filteredString.charAt(filteredString.length()
-                - 1))
-            return isPalindrome(filteredString.substring(1, filteredString.length() -
-                    1));
+        if (filteredString.charAt(0) == filteredString.charAt(filteredString.length() - 1))
+            return isPalindrome(filteredString.substring(1, filteredString.length() - 1));
         return false;
     }
 }
