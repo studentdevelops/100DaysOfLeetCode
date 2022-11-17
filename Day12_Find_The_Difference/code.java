d26f3a8edc0d1a17274ceb0192f69bf2ee12e067
Sourav Kumar <31382363+studentdevelops@users.noreply.github.com>
2022-11-18 00:07:02 +0530

    Create code.java

---
+++ Day12_Find_The_Difference/code.java
@@ -0,0 +1,19 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char[] charArray1 = s.toCharArray();
+        Arrays.sort(charArray1);
+        char[] charArray2 = t.toCharArray();
+        Arrays.sort(charArray2);
+
+        int i = 0, j = 0;
+        while (i< s.length() && j < t.length()) {
+            if (charArray1[i] != charArray2[j]) {
+                return charArray2[j];
+            } else {
+                i++;
+                j++;
+            }
+        }
+        return charArray2[charArray2.length - 1];
+    }
+}
