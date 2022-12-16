709e9034d74699d3d91eed849c647ed01994e797
Sourav Kumar <31382363+studentdevelops@users.noreply.github.com>
2022-12-17 01:08:23 +0530

    Create code.java

---
+++ Day41_Multiply_Values_By_2/code.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        Arrays.sort(nums);
+        for (int i : nums) {
+            if(i==original){
+                original = original*2;
+            }
+        }
+        return original;
+    }
+}
