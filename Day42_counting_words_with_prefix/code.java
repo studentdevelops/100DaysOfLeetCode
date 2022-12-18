a1c445c57a81196bb69381fb3b2157945f2032b2
Sourav Kumar <31382363+studentdevelops@users.noreply.github.com>
2022-12-18 23:48:56 +0530

    Create code.java

---
+++ Day42_counting_words_with_prefix/code.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        for (String string : words) {
+            if(string.startsWith(pref))
+                count++;
+        }
+        return count;
+    }
+}
